4012b896386f4546ff05aadd395df60da1403075
Kristijan34 <kristijan_lazarovski@hotmail.com>
2020-09-21 12:24:20 +0200

    Updates

---
+++ precache-manifest.09f8c6de84d0f24e026d0af6aa50f870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e63c2b4c020429e20b126118c750c7",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "e07c6020b3feaeedc490",
+    "url": "/React/static/css/main.11c7b880.chunk.css"
+  },
+  {
+    "revision": "30347635067341e3fe38",
+    "url": "/React/static/js/2.632019b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.632019b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07c6020b3feaeedc490",
+    "url": "/React/static/js/main.67b8eae4.chunk.js"
+  },
+  {
+    "revision": "94c9419b7ba631dec2f0",
+    "url": "/React/static/js/runtime-main.1ed434bd.js"
+  }
+]);
